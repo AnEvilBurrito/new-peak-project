598461a7e722e80b8b49571abb5708ef661c0ba6
Dawson <ylan0001@student.monash.edu>
2022-08-30 14:03:40 +1000

    bionetgen, inv pysb

--- run_bionetgen.py
+++ run_bionetgen.py
@@ -3,3 +3,4 @@ import sys
 
 # print(sys.path)
 result = bionetgen.run("C:\Github/new-peak-project/bionetgen_example.bngl", 'example')
+# result = bionetgen.run("C://Github//new-peak-project//src//models//bionetgen//peak3.bngl", 'C://Github//new-peak-project//src//models//bionetgen')

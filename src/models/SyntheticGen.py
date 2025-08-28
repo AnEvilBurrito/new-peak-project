74999e0bcfe63deb1e0fa946b93351917f1a3377
Dawson Lan <47137689+AnEvilBurrito@users.noreply.github.com>
2025-08-28 16:49:26 +1000

    Further explorations | variance analysis of generated model simulations, installed tslearn.

--- src/models/SyntheticGen.py
+++ src/models/SyntheticGen.py
@@ -31,6 +31,70 @@ def lhs(n_samples, n_features, random_state=None):
     return result
 
 
+def generate_feature_data_v3(model_spec: ModelSpecification, initial_values: dict, perturbation_type: str, perturbation_params, n, seed=None):
+    '''
+    Generate a dataframe of perturbed values for the model, version 2, supports lhs sampling
+        model_spec: ModelSpecification object
+        initial_values: dict, the initial values of the model
+        perturbation_type: str, the type of perturbation to apply, either 'uniform', 'gaussian', or 'lhs'
+        perturbation_params: dict of parameters for the perturbation, for
+            'lhs' perturbation, the params are {'min': float, 'max': float} - lhs is latin hypercube sampling
+            'uniform' perturbation, the params are {'min': float, 'max': float}
+            'gaussian' perturbation, the param is either {'std': float} or {'rsd': float}
+                'rsd' is the relative standard deviation of the perturbation, i.e. std/mean
+        n: int, the number of perturbations to generate
+        seed: int, the random seed to use for reproducibility
+    Returns:
+        feature_df: dataframe of the perturbed values
+    '''
+    if perturbation_type not in ['uniform', 'gaussian', 'lhs']:
+        raise ValueError('Perturbation type must be "uniform", "gaussian", or "lhs"')
+
+    if perturbation_type in ['uniform', 'lhs']:
+        if 'min' not in perturbation_params or 'max' not in perturbation_params:
+            raise ValueError('For uniform/lhs perturbation, parameters must contain "min" and "max"')
+    elif perturbation_type == 'gaussian':
+        if 'std' not in perturbation_params and 'rsd' not in perturbation_params:
+            raise ValueError('For gaussian perturbation, parameters must contain "std" or "rsd"')
+
+    all_species = initial_values.keys()
+
+    if perturbation_type == 'lhs':
+        min_ = perturbation_params['min']
+        max_ = perturbation_params['max']
+
+        # Initialize LHS sampler
+        sampler = qmc.LatinHypercube(d=len(all_species), seed=seed)
+        lhs_samples = sampler.random(n=n)
+
+        # Scale to [min_, max_] across all dimensions
+        scaled_samples = qmc.scale(lhs_samples, [min_] * len(all_species), [max_] * len(all_species))
+
+        # Build dataframe
+        feature_df = pd.DataFrame(scaled_samples, columns=all_species)
+        return feature_df
+
+    # For uniform and gaussian sampling
+    all_perturbed_values = []
+    for _ in range(n):
+        perturbed_values = {}
+        if perturbation_type == 'uniform':
+            min_ = perturbation_params['min']
+            max_ = perturbation_params['max']
+            for s in all_species:
+                perturbed_values[s] = initial_values[s] * np.random.uniform(min_, max_)
+        elif perturbation_type == 'gaussian':
+            for s in all_species:
+                mu = initial_values[s]
+                sigma = perturbation_params.get('std', perturbation_params.get('rsd', 0) * mu)
+                perturbed_values[s] = np.random.normal(mu, sigma)
+        all_perturbed_values.append(perturbed_values)
+
+    feature_df = pd.DataFrame(all_perturbed_values)
+    return feature_df
+
+
+
 def generate_feature_data_v2(model_spec: ModelSpecification, initial_values: dict, perturbation_type: str, perturbation_params, n, seed=None):
     '''
     Generate a dataframe of perturbed values for the model, version 2, supports lhs sampling
@@ -325,7 +389,11 @@ def generate_target_data_diff_build(model_spec: ModelSpecification,
 
 
 
-def generate_target_data(model_spec: ModelSpecification, solver: Solver, feature_df: pd.DataFrame, simulation_params={'start': 0, 'end': 500, 'points': 100}, n_cores=1, verbose=False):
+def generate_target_data(model_spec: ModelSpecification, solver: Solver, feature_df: pd.DataFrame, 
+                         simulation_params={'start': 0, 'end': 500, 'points': 100}, 
+                         n_cores=1,
+                         outcome_var='Cp', 
+                         verbose=False):
     '''
     Generate the target data for the model
         model_spec: ModelSpecification object   
@@ -364,8 +432,8 @@ def generate_target_data(model_spec: ModelSpecification, solver: Solver, feature
         # on the very next call to simulate, it will reset the model to the original state
         res = solver.simulate(start, end, points) 
         # locate the Cp value in the dataframe
-        Cp = res['Cp'].iloc[-1]
-        time_course = res['Cp'].values
+        Cp = res[outcome_var].iloc[-1]
+        time_course = res[outcome_var].values
         return Cp, time_course
     
     # use parallel processing to speed up the simulation
@@ -396,13 +464,13 @@ def generate_target_data(model_spec: ModelSpecification, solver: Solver, feature
             # on the very next call to simulate, it will reset the model to the original state
             res = solver.simulate(start, end, points) 
             # locate the Cp value in the dataframe
-            Cp = res['Cp'].iloc[-1]
+            Cp = res[outcome_var].iloc[-1]
             all_perturbed_results.append(Cp)
             
             # store the run of Cp into time_course_data
-            time_course_data.append(res['Cp'].values)
+            time_course_data.append(res[outcome_var].values)
             
-    target_df = pd.DataFrame(all_perturbed_results, columns=['Cp'])
+    target_df = pd.DataFrame(all_perturbed_results, columns=[outcome_var])
     return target_df, time_course_data
 
 
